a5e4b820766967cb297019608d87dddca1e15aa7
Emmanuel <wamuoe@gmail.com>
2018-05-14 05:44:44 +0100

    Added basic camera ability

--- app/src/main/java/com/mannysight/saybaba/view/AnimTabsView.java
+++ app/src/main/java/com/mannysight/saybaba/view/AnimTabsView.java
@@ -8,14 +8,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.mannysight.saybaba.R;
 
 public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeListener {
+
 
     private ImageView mCameraView;
     private ImageView mStartView;
@@ -26,6 +29,9 @@ public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeL
     private View mIndicator;
     private int centerColor;
     private int endsColor;
+    private int mEndViewsTranslation;
+    private int mIndicatorTranslationX;
+    private int mCenterTranslationY;
 
     public AnimTabsView(@NonNull Context context) {
         this(context, null);
@@ -41,6 +47,10 @@ public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeL
     }
 
 
+    public ImageView getmCameraView() {
+        return mCameraView;
+    }
+
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.view_anim_tabs, this, true);
         mCameraView = findViewById(R.id.center_image);
@@ -52,10 +62,43 @@ public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeL
         centerColor = ContextCompat.getColor(getContext(), R.color.white);
         endsColor = ContextCompat.getColor(getContext(), R.color.dark_grey);
         argbEvaluator = new ArgbEvaluator();
+
+
+        mIndicatorTranslationX = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                80, getResources().getDisplayMetrics());
+
+        mBottomView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mEndViewsTranslation = (int) (mBottomView.getX() - mStartView.getX() - mIndicatorTranslationX);
+                mCenterTranslationY = getHeight() - mBottomView.getBottom();
+                mBottomView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+
     }
 
-    public void setUpWithViewPager(ViewPager viewPager) {
+    public void setUpWithViewPager(final ViewPager viewPager) {
+
         viewPager.addOnPageChangeListener(this);
+
+        mStartView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem() != 0) {
+                    viewPager.setCurrentItem(0);
+                }
+            }
+        });
+        mEndView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem() != 2) {
+                    viewPager.setCurrentItem(2);
+                }
+            }
+        });
     }
 
 
@@ -63,8 +106,14 @@ public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeL
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
         if (position == 0) {
             setColor(1 - positionOffset);
+            moveViews(1 - positionOffset);
+            moveAndScaleCenter(1 - positionOffset);
+            mIndicator.setTranslationX((positionOffset - 1) * mIndicatorTranslationX);
         } else if (position == 1) {
             setColor(positionOffset);
+            moveViews(positionOffset);
+            moveAndScaleCenter(positionOffset);
+            mIndicator.setTranslationX(positionOffset * mIndicatorTranslationX);
         }
     }
 
@@ -76,6 +125,27 @@ public class AnimTabsView extends FrameLayout implements ViewPager.OnPageChangeL
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    private void moveAndScaleCenter(float fractionFromCenter) {
+        float scale = .7f + ((1 - fractionFromCenter) * .3f);
+
+        mCameraView.setScaleX(scale);
+        mCameraView.setScaleY(scale);
+
+        int translation = (int) (fractionFromCenter * mCenterTranslationY);
+
+        mCameraView.setTranslationY(translation);
+        mBottomView.setTranslationY(translation);
+        mBottomView.setAlpha(1 - fractionFromCenter);
+    }
+
+    private void moveViews(float fractionFromFromCentre) {
+        mStartView.setTranslationX(fractionFromFromCentre * mEndViewsTranslation);
+        mEndView.setTranslationX(-fractionFromFromCentre * mEndViewsTranslation);
+
+        mIndicator.setAlpha(fractionFromFromCentre);
+        mIndicator.setScaleX(fractionFromFromCentre);
     }
 
     private void setColor(float fractionFromCenter) {
